f298a259c9cc3d1c0a28d77b11cc509e13625ed4
itsiammk <iammk105209@gmail.com>
2022-06-17 00:51:26 +0530

    koding

---
+++ src/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <div>
+      <footer>
+        <div class="footer-content">
+            <h3>Mahesh K</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo iste corrupti doloribus odio sed!</p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-github"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>copyright &copy;2020 MaheshK. designed by <span>MaheshK</span></p>
+        </div>
+    </footer>
+      </div>
+    )
+}
